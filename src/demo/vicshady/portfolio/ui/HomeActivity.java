9d9c135fa5807cff0b955a163883c8cf195279db
vikalppatelce <vikalppatelce@yahoo.com>
2014-02-08 23:44:03 +0530

    08/02/14 -- 1. Added Preference Screen [Change Log]. 2. Added HomeScreen Layout. 3. Added Action Bar Sherlock with Settings Option for now. --

--- src/demo/vicshady/portfolio/ui/HomeActivity.java
+++ src/demo/vicshady/portfolio/ui/HomeActivity.java
@@ -1,53 +1,47 @@
 package demo.vicshady.portfolio.ui;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
-import android.widget.LinearLayout.LayoutParams;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
 import demo.vicshady.portfolio.R;
 
-public class HomeActivity extends Activity{
+public class HomeActivity extends SherlockFragmentActivity{
 
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		TextView uppertext = new TextView(this);
-    	uppertext.setText("UpperText");
-    	//uppertext.setGravity(Gravity.CENTER_HORIZONTAL);
-    	uppertext.setTextColor(Color.GRAY);
-    	uppertext.setTypeface(null, Typeface.ITALIC);
-    	uppertext.setTextSize(15 * getResources().getDisplayMetrics().density);
-    	uppertext.setPadding(20, 0, 20, 0);
-    	LinearLayout.LayoutParams upperTextParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        
-    	ImageView img = new ImageView(this);
-        img.setImageResource(R.drawable.update_tour_phone0);
-        img.setScaleType(ScaleType.CENTER_CROP);
-        //img.setPadding(20, 0, 20, 0);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(20, 0, 20, 0);
-        
-        TextView lowertext = new TextView(this);
-        lowertext.setText("lowertext");
-        lowertext.setTextColor(Color.GRAY);
-        lowertext.setTypeface(null, Typeface.ITALIC);
-        lowertext.setTextSize(10 * getResources().getDisplayMetrics().density);
-        lowertext.setPadding(20, 0, 20, 0);
-        LinearLayout.LayoutParams lowerTextParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);        
-
-        LinearLayout layout = new LinearLayout(this);
-        layout.setLayoutParams(new LayoutParams(android.view.ViewGroup.LayoutParams.FILL_PARENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-        layout.setOrientation(1);
-        layout.addView(uppertext,upperTextParams);
-        layout.addView(img,layoutParams);
-        layout.addView(lowertext,lowerTextParams);
-        setContentView(layout);
+		setContentView(R.layout.home);
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		
+		MenuInflater inflater = getSupportMenuInflater();
+	       inflater.inflate(R.menu.main, menu);
+		 return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+        case R.id.more: 
+        	//Intent iSettings = new Intent(this,PrefsActivity.class);
+        	//startActivity(iSettings);
+        	return true;
+        case R.id.action_settings:
+        	Intent iSettings = new Intent(this,PrefsActivity.class);
+        	startActivity(iSettings);
+        	return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
---
+++ src/demo/vicshady/portfolio/ui/PrefsActivity.java
@@ -0,0 +1,90 @@
+package demo.vicshady.portfolio.ui;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+import demo.vicshady.portfolio.R;
+import demo.vicshady.portfolio.ui.utils.ChangeLogDialog;
+
+public class PrefsActivity extends SherlockPreferenceActivity implements OnSharedPreferenceChangeListener {
+	
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		PreferenceManager prefMgr = getPreferenceManager();
+		addPreferencesFromResource(R.xml.settings);
+		Preference release = prefMgr.findPreference("prefRelease");
+		if(release!=null)
+		{
+			release.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+				@Override
+				public boolean onPreferenceClick(Preference preference) {
+					// TODO Auto-generated method stub
+					ChangeLogDialog changeLogDialog = new ChangeLogDialog(PrefsActivity.this);
+					changeLogDialog.show();
+					return false;
+				}
+			});
+		}
+		
+		Preference dev = prefMgr.findPreference("prefDev");
+		if(dev!=null)
+		{
+			dev.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+				
+				@Override
+				public boolean onPreferenceClick(Preference preference) {
+					// TODO Auto-generated method stub
+				//	showDialog(DEV);
+					copyDatabase();
+					return false;
+				}
+			});
+		}
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		// TODO Auto-generated method stub
+	}
+	
+	public void copyDatabase()
+	{
+		try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String currentDBPath = "/data/data/" + getPackageName() + "/databases/PatientDB";
+                String backupDBPath = "PatientDB_Dev.db";
+                File currentDB = new File(currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(currentDB).getChannel();
+                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                    Toast.makeText(getApplicationContext(), "Database Transfered!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+        	Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+	}
+}
